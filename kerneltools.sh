0dc90e34bbda53dd6146165dc15130f5155769b3
Erik142 <EWahlberger@gmail.com>
2013-08-14 14:56:32 +0200

    Added functionality to automatically delete old kernels from the system, also cleaned up code

--- kerneltools.sh
+++ kerneltools.sh
@@ -18,6 +18,16 @@ fi
 
 
 
+#Remove Can either be declared as none, subversion, patchlevel or all!
+#Kernel name should be configured to name the kernel basename!
+REMOVE="patchlevel"
+KERNEL_NAME="vmlinuz"
+#The boot path is the path in which the kernel is located.
+BOOT_PATH=/boot/
+#The STD version is the substring after the - symbol that the distributions default-kernel contains.
+STD_VERSION="linux"
+
+#DO NOT CHANGE THE FOLLOWING CODE OR THE SCRIPT WILL BREAK!
 
 LOCAL_VERSION=""
 SRC_DIR="/usr/src/"
@@ -35,6 +45,7 @@ BRCM_FLAG=0
 VBOX_FLAG=0
 CK_FLAG=0
 VERBOSE_FLAG=0
+KERNEL_PATH="$BOOT_PATH$KERNEL_NAME-"
 
 declare -a argArray
 x=0
@@ -141,6 +152,8 @@ w=${array[0]}
 const=0
 y=${array[1]}
 z=${array[2]}
+
+#unset array
 
 if [ $z -eq 0 ]
 then
@@ -369,7 +382,7 @@ fi
 
 if [ $CK_FLAG -eq 1 ]
 	then
-	if [ $CK -ne 0]
+	if [ $CK -ne 0 ]
 	then
 	echo "Found ck version $w.$y-ck$CK!"
 	fi
@@ -434,9 +447,9 @@ fi
 
 CK_VERSION="patch-$w.$y-ck$CK"
 
-if [ ! -f "patch-$CK_VERSION" ]
+if [ ! -f "$CK_VERSION" ]
 then
-	bunzip2 -k ""patch-$CK_VERSION".bz2"
+	bunzip2 -k ""$CK_VERSION".bz2"
 	fnCheckSuccess
 fi
 fi
@@ -462,7 +475,7 @@ fnCheckSuccess
 
 	echo "Patching source code..."
 	cd "$SRC_DIR$SOURCE_FOLDER-ck$CK"
-	patch -p1 < $SRC_DIR/"patch-$CK_VERSION"
+	patch -p1 < $SRC_DIR$CK_VERSION
 	fnCheckSuccess
 
 fi
@@ -552,9 +565,27 @@ fi
 	LOCAL_VERSION=$(printf $LOCAL_VERSION | sed 's/=//g')
 	LOCAL_VERSION=$(printf $LOCAL_VERSION | sed 's/\"//g')
 
+	TMPLOCAL=$(echo $LOCAL_VERSION | sed 's/^-\(.*\)/\1/')
+
 	if [ $CK_FLAG -eq 1 ]
 	then
-	LOCAL_VERSION="-ck$CK$LOCAL_VERSION"
+
+	if [[ $TMPLOCAL != *ck* ]]
+		then
+		CKLOCAL="ck$CK-"$TMPLOCAL
+
+		sed -i "s/$TMPLOCAL/$CKLOCAL/g" $SRC_DIR$SOURCE_FOLDER/.config
+
+		LOCAL_VERSION="-$CKLOCAL"
+	fi
+
+	else
+		if [[ $TMPLOCAL == *ck* ]]
+			then
+			NORMLOCAL=${TMPLOCAL:3}
+			sed -i "s/$TMPLOCAL/$NORMLOCAL/g" $SRC_DIR$SOURCE_FOLDER/.config
+		fi
+	
 	fi
 
 
@@ -595,9 +626,9 @@ fi
 		fnCheckSuccess
 	fi
 
-	if [ -f /boot/vmlinuz-$VERSION$LOCAL_VERSION ]
+	if [ -f $KERNEL_PATH$VERSION$LOCAL_VERSION ]
 	then
-		rm /boot/vmlinuz-$VERSION$LOCAL_VERSION
+		rm $KERNEL_PATH$VERSION$LOCAL_VERSION
 		fnCheckSuccess
 	fi
 	
@@ -609,45 +640,361 @@ fi
 	echo "Removing previous kernel"
 	
 
-	RM_VERSION=$w.$y.$((z-2))
+declare -a kernelArray
+x=0
 
+for i in $(find /boot -maxdepth 1 -type f -name $KERNEL_NAME-\* | sort --version-sort)
+do
+tempvar=$i
+tempvar=${tempvar#${KERNEL_PATH}}
+tempvar=${tempvar#${STD_VERSION}}
+#$tempvar=$(echo $tempvar | tr -d ' ')
 
-	if [ -f /boot/vmlinuz-$RM_VERSION$LOCAL_VERSION ]
+if [ -n "$tempvar" ] 
+then
+kernelArray[$x]=$tempvar
+fi
+x=$((x+1))
+done
+
+#echo ${#kernelArray[@]}
+
+echo "These are the kernels currently stored in the kernelArray:"
+echo ""
+
+k=0
+
+while [ $k -lt ${#kernelArray[@]} ]
+do
+	if [ -n ${kernelArray[$k]} ]
 	then
-	rm /boot/vmlinuz-$RM_VERSION$LOCAL_VERSION
-	fnCheckSuccess	
+	echo $((k+1))": ${kernelArray[$k]}"
 	fi
+	k=$((k+1))
+done
+
+echo ""
+
+#CURRENT_VERSION=$VERSION
+#CURRENT_VERSION=$(printf "$CURRENT_VERSION" | sed 's/[a-zA-Z]//g')
+#CURRENT_VERSION=${CURRENT_VERSION//-/$'\n'}
+CURRENT_VERSION=${CURRENT_VERSION//./$'\n'}
+
+declare -a currentarray
+x=0
+
+for i in $CURRENT_VERSION
+do
+currentarray[$x]=$i
+x=$((x+1))
+done
+
+w=${currentarray[0]}
+y=${currentarray[1]}
+z=${currentarray[2]}
+
+#unset currentarray
+
+x=0
+
+case $REMOVE in 
+	"all")
+	echo "Removing all versions lower than compiled kernel!"
+	while [ $x -lt ${#kernelArray[@]} ]
+	do
+	TEMPVERSION=""
+	TEMP_VERSION=${kernelArray[$x]}
+	TEMP_VERSION=$(printf "$TEMP_VERSION" | sed 's/[a-zA-Z]//g')
+	TEMP_VERSION=${TEMP_VERSION//-/$'\n'}
+	TEMP_VERSION=${TEMP_VERSION//./$'\n'}
+
+	declare -a temparray
+	j=0
+
+	for i in $TEMP_VERSION
+	do
+	temparray[$j]=$i
+	j=$((j+1))
+	done
+
+	tempx=${temparray[0]}
+	tempy=${temparray[1]}
+	tempz=${temparray[2]}
+
+	unset temparray
+
+
+	if (( "$tempx" < "$w" ))
+	then
+	if [ -f "$BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}" ]
+		then
+		echo "Removing $BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				rm "$BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				fnCheckSuccess
+				fi
+		
+				if [ -f $BOOT_PATH"initrd-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initrd-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
+
+				if [ -f $BOOT_PATH"initramfs-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initramfs-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
+
+				if [ -d /lib/modules/${kernelArray[$x]} ]
+				then
+				rm -r /lib/modules/${kernelArray[$x]}
+				fnCheckSuccess
+				fi
+
+				if [ -d $SRC_DIR"linux-$tempx.$tempy.$tempz" ]
+				then
+				rm -r $SRC_DIR"linux-$tempx.$tempy.$tempz"
+				fnCheckSuccess
+				fi
+
+				if [ -f $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz" ]
+				then
+				rm $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz"
+				fi
 	
-	if [ -f /boot/initrd-$RM_VERSION$LOCAL_VERSION.img ]
+	else
+		if (( "$tempy" < "$y" ))
+		then
+				if [ -f $BOOTPATH$KERNEL_NAME${kernelArray[$x]} ]
+				then
+				echo "Removing $BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				rm "$BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				fnCheckSuccess
+				fi
+		
+				if [ -f $BOOT_PATH"initrd-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initrd-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
+
+				if [ -f $BOOT_PATH"initramfs-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initramfs-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
+
+				if [ -d /lib/modules/${kernelArray[$x]} ]
+				then
+				rm -r /lib/modules/${kernelArray[$x]}
+				fnCheckSuccess
+				fi
+
+				if [ -d $SRC_DIR"linux-$tempx.$tempy.$tempz" ]
+				then
+				rm -r $SRC_DIR"linux-$tempx.$tempy.$tempz"
+				fnCheckSuccess
+				fi
+
+				if [ -f $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz" ]
+				then
+				rm $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz"
+				fi	
+
+		else
+			if (( "$tempz" < "$z" ))
+			then
+				if [ -f $BOOTPATH$KERNEL_NAME${kernelArray[$x]} ]
+				then
+				echo "Removing $BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				rm "$BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				fnCheckSuccess
+				fi
+		
+				if [ -f $BOOT_PATH"initrd-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initrd-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
+
+				if [ -f $BOOT_PATH"initramfs-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initramfs-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
+
+				if [ -d /lib/modules/${kernelArray[$x]} ]
+				then
+				rm -r /lib/modules/${kernelArray[$x]}
+				fnCheckSuccess
+				fi
+
+				if [ -d $SRC_DIR"linux-$tempx.$tempy.$tempz" ]
+				then
+				rm -r $SRC_DIR"linux-$tempx.$tempy.$tempz"
+				fnCheckSuccess
+				fi
+
+				if [ -f $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz" ]
+				then
+				rm $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz"
+				fi
+	
+			else
+			echo "Kernel version is not lower than compiled one!"
+			fi
+		fi
+	fi
+	x=$((x+1))
+	done
+	;;
+	"patchlevel")
+	echo "Removing all versions lower than local patchlevel"
+	while [ $x -lt ${#kernelArray[@]} ]
+	do
+	TEMPVERSION=""
+	TEMP_VERSION=${kernelArray[$x]}
+	TEMP_VERSION=$(printf "$TEMP_VERSION" | sed 's/[a-zA-Z]//g')
+	TEMP_VERSION=${TEMP_VERSION//-/$'\n'}
+	TEMP_VERSION=${TEMP_VERSION//./$'\n'}
+
+	declare -a temparray
+	j=0
+
+	for i in $TEMP_VERSION
+	do
+	temparray[$j]=$i
+	j=$((j+1))
+	done
+
+	tempx=${temparray[0]}
+	tempy=${temparray[1]}
+	tempz=${temparray[2]}
+
+	unset temparray
+
+	
+	if (( "$tempy" < "$y" ))
 	then
-	rm /boot/initrd-$RM_VERSION$LOCAL_VERSION.img
-	fnCheckSuccess
-	fi
+	if [ -f "$BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}" ]
+				then
+				echo "Removing $BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				rm "$BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				fnCheckSuccess
+				fi
+		
+				if [ -f $BOOT_PATH"initrd-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initrd-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
 
-	if [ -f /boot/initramfs-$RM_VERSION$LOCAL_VERSION.img ]
-	then
-	rm /boot/initramfs-$RM_VERSION$LOCAL_VERSION.img
-	fnCheckSuccess
-	fi
+				if [ -f $BOOT_PATH"initramfs-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initramfs-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
 
-	if [ -d /lib/modules/$RM_VERSION$LOCAL_VERSION ]
+				if [ -d /lib/modules/${kernelArray[$x]} ]
+				then
+				rm -r /lib/modules/${kernelArray[$x]}
+				fnCheckSuccess
+				fi
+
+				if [ -d $SRC_DIR"linux-$tempx.$tempy.$tempz" ]
+				then
+				rm -r $SRC_DIR"linux-$tempx.$tempy.$tempz"
+				fnCheckSuccess
+				fi
+
+				if [ -f $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz" ]
+				then
+				rm $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz"
+				fi
+	
+	fi
+	x=$((x+1))
+	done
+	;;
+	"subversion")
+	echo "Removing all versions lower than local subversion"
+	while [ $x -lt ${#kernelArray[@]} ]
+	do
+	TEMPVERSION=""
+	TEMP_VERSION=${kernelArray[$x]}
+	TEMP_VERSION=$(printf "$TEMP_VERSION" | sed 's/[a-zA-Z]//g')
+	TEMP_VERSION=${TEMP_VERSION//-/$'\n'}
+	TEMP_VERSION=${TEMP_VERSION//./$'\n'}
+
+	declare -a temparray
+	j=0
+
+	for i in $TEMP_VERSION
+	do
+	temparray[$j]=$i
+	j=$((j+1))
+	done
+
+	tempx=${temparray[0]}
+	tempy=${temparray[1]}
+	tempz=${temparray[2]}
+
+	unset temparray
+
+
+	if [ "$tempy" -eq "$y" ]
+	then	
+		if (( "$tempz" < "$z" ))
 		then
-		rm -r /lib/modules/$RM_VERSION$LOCAL_VERSION
-		fnCheckSuccess
-	fi
+		if [ -f "$BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}" ]
+				then
+				echo "Removing $BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				rm "$BOOT_PATH$KERNEL_NAME-${kernelArray[$x]}"
+				fnCheckSuccess
+				fi
+		
+				if [ -f $BOOT_PATH"initrd-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initrd-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
 
-	if [ -d /usr/src/linux-$RM_VERSION ]
-		then
-		rm -r /usr/src/linux-$RM_VERSION
-		rm -r /usr/src/linux-$RM_VERSION-ck$CK
-		fnCheckSuccess
-	fi
+				if [ -f $BOOT_PATH"initramfs-${kernelArray[$x]}.img" ]
+				then
+				rm $BOOT_PATH"initramfs-${kernelArray[$x]}.img"
+				fnCheckSuccess
+				fi
 
-	if [ -f /usr/src/linux-$RM_VERSION.tar.xz ]
-		then
-		rm /usr/src/linux-$RM_VERSION.tar.xz
-	fi
+				if [ -d /lib/modules/${kernelArray[$x]} ]
+				then
+				rm -r /lib/modules/${kernelArray[$x]}
+				fnCheckSuccess
+				fi
 
+				if [ -d $SRC_DIR"linux-$tempx.$tempy.$tempz" ]
+				then
+				rm -r $SRC_DIR"linux-$tempx.$tempy.$tempz"
+				fnCheckSuccess
+				fi
+
+				if [ -f $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz" ]
+				then
+				rm $SRC_DIR"linux-$tempx.$tempy.$tempz.tar.xz"
+				fi
+	
+		fi
+	fi
+	x=$((x+1))
+	done
+	;;
+	"none")
+	echo "Removing none"
+	;;
+	*)
+	echo "Undefined Option for removal..."
+	exit 1
+	;;
+esac 
 
 
 
@@ -656,7 +1003,7 @@ fi
 	then
 	VERSION="$w.$y.$z"
 	fi
-	cp -v $SRC_DIR$SOURCE_FOLDER/arch/x86/boot/bzImage /boot/vmlinuz-$VERSION$LOCAL_VERSION
+	cp -v $SRC_DIR$SOURCE_FOLDER/arch/x86/boot/bzImage $KERNEL_PATH$VERSION$LOCAL_VERSION
 
 	if [ ! -d /lib/modules/$VERSION$LOCAL_VERSION/build ]
 	then
@@ -671,7 +1018,7 @@ fi
 	fnCheckSuccess
 	fi
 
-	if [ $BRCM_FLAG -eq 1 ]
+if [ $BRCM_FLAG -eq 1 ]
 	then
 	echo "Installing new broadcom-wl..."
 	dkms install $BRCMVERSION -k $VERSION$LOCAL_VERSION
